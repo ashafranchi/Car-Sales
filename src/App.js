1b26a9be97184a68738dc8588da6d251d904f540
Asha <asha17@brown.edu>
2019-08-15 14:08:38 -0700

    mvp

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import Header from './components/Header';
-import AddedFeatures from './components/AddedFeatures';
-import AdditionalFeatures from './components/AdditionalFeatures';
-import Total from './components/Total';
+import Header from "./components/Header";
+import AddedFeatures from "./components/AddedFeatures";
+import AdditionalFeatures from "./components/AdditionalFeatures";
+import Total from "./components/Total";
 
-import {addFeature, deleteFeature} from "./actions/a-index";
+import { addFeature, deleteFeature } from "./actions/a-index";
 
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 
 const App = props => {
+  console.log("props", props);
   return (
     <div className="boxes">
       <div className="box">
@@ -17,11 +18,21 @@ const App = props => {
         <AddedFeatures car={props.car} delete={props.deleteFeature} />
       </div>
       <div className="box">
-        <AdditionalFeatures store={props.store} addFeature={props.addFeature}/>
+        <AdditionalFeatures store={props.store} addFeature={props.addFeature} />
         <Total />
       </div>
     </div>
   );
 };
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    car: state.car,
+    store: state.store
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { addFeature, deleteFeature }
+)(App);
